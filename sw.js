259715b6cf77e79a4705b78abe1fb42e6817c4a3
mohaehlb-boop <moha.ehlb@gmail.com>
2025-12-21 12:28:37 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,1 @@
-self.addEventListener('push', function(event) {
-    const options = {
-        body: event.data ? event.data.text() : 'Nouvelle notification personnalisée !',
-        icon: 'https://cdn-icons-png.flaticon.com/512/2331/2331970.png',
-        badge: 'https://cdn-icons-png.flaticon.com/512/2331/2331970.png'
-    };
-    event.waitUntil(
-        self.registration.showNotification('Ma Boutique', options)
-    );
-});
+
